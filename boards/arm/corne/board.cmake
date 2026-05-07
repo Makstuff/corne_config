294280bbf71bf0d639a2eab6b91c33fa56578e86
Makstuff <mxmln.dmbrgr@gmail.cwm>
2026-05-07 03:58:30 +0200

    feat: add German keyboard layout definitions and Corne board configuration files

--- boards/arm/corne/board.cmake
+++ boards/arm/corne/board.cmake
@@ -1,6 +1,3 @@
-# Copyright (c) 2021 The ZMK Contributors
-# SPDX-License-Identifier: MIT
-
 set(OPENOCD_NRF5_SUBFAMILY nrf52)
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
