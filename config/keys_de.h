ed91f37a0f84f1e49db48a952d0afd97d128c2e0
Makstuff <57408125+Makstuff@users.noreply.github.com>
2026-09-25 12:43:14 +0200

    fix: reset ZMK upgrade and restore kps macros for dead keys

--- config/keys_de.h
+++ config/keys_de.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#include <dt-bindings/zmk/hid_usage.h>
-#include <dt-bindings/zmk/hid_usage_pages.h>
+#include <dt-bindings/zmk/keys.h>
 #include <dt-bindings/zmk/modifiers.h>
 
 #define DE_ESCAPE_CHARACTER                                                    \
